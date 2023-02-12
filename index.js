86cf6bb927e8554241e4e5f642f2b9e52dbf5d74
Andranik Markaryan <andranik.mrk@gmail.com>
2023-02-12 17:46:16 +0000

    Figure out noodle tangency to bowl

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const canvasCenter = {
   y: canvasSize / 2,
 };
 const pi = Math.PI;
+const bowlLineWidth = 10;
+const noodleLineWidth = 2;
 
 /**
  *
@@ -16,27 +18,29 @@ function makeDrawing(noodleWidth) {
   if (canvas.getContext) {
     ctx = canvas.getContext("2d");
     setupCanvas();
-    drawBowl();
+    const bowlRadius = drawBowl();
 
     ctx.strokeStyle = "blue";
-    ctx.lineWidth = 1;
-    drawNoodle(noodleWidth);
+    ctx.lineWidth = noodleLineWidth;
+    drawNoodle(bowlRadius, noodleWidth);
   }
 }
 
-function drawNoodle(noodleWidth) {
+function drawNoodle(bowlRadius, noodleWidth) {
   let minBendRadius = 50;
   let maxBendRadius = 20;
   let maximumNextAngleFactor = 0.5;
 
-  let arcCenterX = canvasCenter.x;
-  let arcCenterY = canvasCenter.y;
-  let radius = minBendRadius;
-  let startAngle = Math.random() * 2 * pi;
-  let endAngle = Math.random() * 2 * pi;
+  const initialRadius = minBendRadius;
+  let [arcCenterX, arcCenterY, startAngle, endAngle] = getNoodleInitialParams(
+    bowlRadius,
+    initialRadius
+  );
+
+  let radius = initialRadius;
   let ccw = false;
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 10; i++) {
     // console.log(`${ccw ? "c" : ""}cw Arc #`, i + 1);
     // console.log(
     //   `Radius ${radius}, x ${arcCenterX}, y ${arcCenterY}, i˚ ${
@@ -84,6 +88,24 @@ function drawNoodle(noodleWidth) {
   }
 }
 
+function getNoodleInitialParams(bowlRadius, initialRadius) {
+  const initialAngleOnBowl = Math.random() * 2 * pi;
+
+  const arcCenterX =
+    canvasCenter.x +
+    (bowlRadius - initialRadius - bowlLineWidth / 2) *
+      Math.cos(initialAngleOnBowl);
+  const arcCenterY =
+    canvasCenter.y +
+    (bowlRadius - initialRadius - bowlLineWidth / 2) *
+      Math.sin(initialAngleOnBowl);
+
+  const startAngle = initialAngleOnBowl - pi / 2;
+  const endAngle = initialAngleOnBowl + pi / 2;
+
+  return [arcCenterX, arcCenterY, startAngle, endAngle];
+}
+
 function degToRad(deg) {
   if (deg > 360) {
     deg = deg % 360;
@@ -104,12 +126,15 @@ function setupCanvas() {
 }
 
 function drawBowl() {
+  const bowlRadius = (0.95 * canvasSize) / 2;
+
   ctx.strokeStyle = "black";
-  ctx.lineWidth = 10;
+  ctx.lineWidth = bowlLineWidth;
   ctx.beginPath();
-  ctx.arc(canvasCenter.x, canvasCenter.y, (0.95 * canvasSize) / 2, 0, 360);
+  ctx.arc(canvasCenter.x, canvasCenter.y, bowlRadius, 0, 360);
   ctx.stroke();
-  ctx.closePath();
+
+  return bowlRadius;
 }
 
 function moveToCenter() {
